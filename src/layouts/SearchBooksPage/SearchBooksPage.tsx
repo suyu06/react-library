71e47b400307bc359a84295ad4207340bb6e7124
suyu06 <suyu.zhang@johnabbottcollege.net>
2023-02-06 14:46:02 -0500

    change the page number and its data dynamically

--- src/layouts/SearchBooksPage/SearchBooksPage.tsx
+++ src/layouts/SearchBooksPage/SearchBooksPage.tsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import BookModel from "../../models/BookModel";
+import { Pagination } from "../Utils/Pagination";
 import { SpinnerLoading } from "../Utils/SpinnerLoading";
 import { SearchBook } from "./components/SearchBook";
 
@@ -10,12 +11,23 @@ export const SerachBooksPage = () => {
   const [isLoading, setIsLoading] = useState(true);
   //API call failure scenario
   const [httpError, setHttpError] = useState(null);
+  // paginaton array
+  const [currentPage, setCurrentPage] = useState(1);
+  // each page show 5 books
+  const [booksPerPage] = useState(5);
+  // total amounts of books
+  const [totalAmountofBooks, setTotalAmountofBooks] = useState(0);
+  // total pages
+  const [totalPages, setTotalPages] = useState(0);
 
   useEffect(() => {
     const fetchBooks = async () => {
       const baseUrl: string = "http://localhost:8080/api/books";
-      // change the query parameter from "size = 9" to "size=5"
-      const url: string = `${baseUrl}?page=0&size=5`;
+      // 1.change the query parameter from "size = 9" to "size=5"
+      // 2.change the query parameter page number from 0 to a  dynamically number
+      const url: string = `${baseUrl}?page=${
+        currentPage - 1
+      }&size=${booksPerPage}`;
       // fetching the url data
       const response = await fetch(url);
       //failure scenario
@@ -24,9 +36,12 @@ export const SerachBooksPage = () => {
       }
       // transfer the url data into json;
       const responseJson = await response.json();
-
       // get the data which is the object of embedded books
       const responseData = responseJson._embedded.books;
+      //get the total amount of books
+      setTotalAmountofBooks(responseJson.page.totalElements);
+      //get the total pages to show these books
+      setTotalPages(responseJson.page.totalPages);
       // create a book array
       const loadedBooks: BookModel[] = [];
       // iterate the object in responseData, push them into the book array,and set the loading process finished.
@@ -50,7 +65,9 @@ export const SerachBooksPage = () => {
       setIsLoading(false);
       setHttpError(error.message);
     });
-  }, []);
+    window.scrollTo(0,0);
+    // pass current page , so each time we click on different page number, the data will be refreshed.
+  }, [currentPage]);
   // when in loading process, show: "Loading..."
   if (isLoading) {
     return <SpinnerLoading />;
@@ -63,6 +80,13 @@ export const SerachBooksPage = () => {
       </div>
     );
   }
+  const indexOfLastBook: number = currentPage * booksPerPage;
+  const indexOfFirstBook: number = indexOfLastBook - booksPerPage;
+  let lastItem =
+    booksPerPage * currentPage <= totalAmountofBooks
+      ? booksPerPage * currentPage
+      : totalAmountofBooks;
+  const paginate = (pageNumber: number) => setCurrentPage(pageNumber);
   return (
     <div>
       <div className="container">
@@ -128,14 +152,18 @@ export const SerachBooksPage = () => {
           </div>
           {/* short description of search result part */}
           <div className="mt-3">
-            <h5> Number of results: (22)</h5>
+            {/* change the original 22 to a dynamical number */}
+            <h5> Number of results: ({totalAmountofBooks})</h5>
           </div>
-          <p>
-            1 to 5 of 22 items :
-          </p>
-          {books.map(book =>(
-            <SearchBook book = {book} key={book.id}/>
+          {/* 1 to 5 of 22 items :change into dynamical number */}
+          <p>{indexOfFirstBook +1} to {lastItem} of {totalAmountofBooks} items :</p>
+          {books.map((book) => (
+            <SearchBook book={book} key={book.id} />
           ))}
+          {/* only render <pagination/> if totalPages >1*/}
+          {totalPages >1 && 
+          <Pagination currentPage={currentPage } totalPages={totalPages} paginate={paginate}/>
+        }
         </div>
       </div>
     </div>
