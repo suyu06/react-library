acea771d7d22a6a7ba58ca851f1b5ecd56b2f1b6
suyu06 <suyu.zhang@johnabbottcollege.net>
2023-02-08 09:47:07 -0500

    create bookCheckoutPage useEffect

--- src/layouts/BookCheckoutPage/BookCheckoutPage.tsx
+++ src/layouts/BookCheckoutPage/BookCheckoutPage.tsx
@@ -1,17 +1,64 @@
-import { useState } from "react"
-import BookModel from "../../models/BookModel"
+import { useEffect, useState } from "react";
+import BookModel from "../../models/BookModel";
+import { SpinnerLoading } from "../Utils/SpinnerLoading";
 
-export const BookCheckoutPage = () =>{
+export const BookCheckoutPage = () => {
+  // create useState
+  const [book, setBook] = useState<BookModel>();
+  const [isLoading, setIsLoading] = useState(true);
+  const [httpError, setHttpError] = useState(null);
+  //add two slashes in the url: http://localhost:3000/checkout/<bookId>
+  const bookId = window.location.pathname.split("/")[2];
 
-    // create useState
-    const [book,setBook] = useState<BookModel>();
-    const [isLoadingBook,setIsLoadingBook] = useState(true);
-    const [httpError,setHttpError] = useState(null);
-    //add two slashes in the url: http://localhost:3000/checkout/<bookId> 
-    const bookId = (window.location.pathname).split('/')[2];
-    return(
-        <div>
-            <h3>HI Library</h3>
-        </div>
-    )
-}
+  useEffect(() => {
+    const fetchBook = async () => {
+      const baseUrl: string = `http://localhost:8080/api/books/${bookId}`;
+
+      // fetching the url data
+      const response = await fetch(baseUrl);
+      //failure scenario
+      if (!response.ok) {
+        throw new Error("Something went wrong!");
+      }
+      // transfer the url data into json;
+      const responseJson = await response.json();
+
+      // create a book object
+      const loadedBook: BookModel = {
+        id: responseJson.id,
+        title: responseJson.title,
+        author: responseJson.author,
+        description: responseJson.description,
+        copies: responseJson.copies,
+        copiesAvailable: responseJson.copiesAvailable,
+        category: responseJson.category,
+        img: responseJson.img,
+      };
+
+      setBook(loadedBook);
+      setIsLoading(false);
+    };
+    // if async has an error
+    fetchBook().catch((error: any) => {
+      setIsLoading(false);
+      setHttpError(error.message);
+    });
+  }, []);
+
+  if (isLoading) {
+    return <SpinnerLoading />;
+  }
+  // if there is error in fetch data
+  if (httpError) {
+    return (
+      <div className="container m-5">
+        <p>{httpError}</p>
+      </div>
+    );
+  }
+  return (
+    <div>
+      <h3>HI Library</h3>
+    </div>
+  );
+};
