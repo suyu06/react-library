7498438058778e7ea79bca767230a71fc70561d5
suyu06 <suyu.zhang@johnabbottcollege.net>
2023-02-11 13:18:00 -0500

    create loginWidget and SignInWidget and add routes to them in the App.tsx

--- src/lib/oktaConfig.ts
+++ src/lib/oktaConfig.ts
@@ -2,9 +2,10 @@ export const oktaConfig={
     //copy client Id from your okta account
     clientId:'0oa8avho11Q6KyNMZ5d7',
     //dev environment url
-    issuer:'http://<dev-65756343.okta.com>/oauth2/default',
-    redirectUri:'http://localhost:3000/login/callback',
-    scopes:['openId','profile','email'],
+    issuer:'https://dev-65756343.okta.com/oauth2/default',
+    redirectUri: 'http://localhost:3000/login/callback',
+    scopes: ['openid', 'profile', 'email'],
+    //Proof Key for Code Exchange
     pkce:true,
-    disableHttpsCheck:true,
+    disableHttpsCheck: true,
 }
