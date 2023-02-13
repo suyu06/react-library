8e06cc16372bcef96dbc1fcd0a2245c0ffd362cb
suyu06 <suyu.zhang@johnabbottcollege.net>
2023-02-13 09:14:59 -0500

    add SecureRoute for our shelf page in APP.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -8,9 +8,10 @@ import { Navbar } from "./layouts/NavbarAndFooter/Navbar";
 import { SearchBooksPage } from "./layouts/SearchBooksPage/SearchBooksPage";
 import { oktaConfig } from "./lib/oktaConfig";
 import { OktaAuth,toRelativeUrl } from "@okta/okta-auth-js";
-import { Security ,LoginCallback} from "@okta/okta-react";
+import { Security ,LoginCallback, SecureRoute} from "@okta/okta-react";
 import LoginWidget from "./Auth/LoginWidget";
 import { ReviewListPage } from "./layouts/BookCheckoutPage/ReviewListPage/ReviewListPage";
+import { ShelfPage } from "./layouts/ShelfPage/ShelfPage";
 
 const oktaAuth = new OktaAuth(oktaConfig);
 
@@ -50,6 +51,7 @@ export const App = () => {
             } 
           />
            <Route path='/login/callback' component={LoginCallback} />
+           <SecureRoute path='/shelf'> <ShelfPage/> </SecureRoute>
         </Switch>
       </div>
       <Footer></Footer>
