fd335a8512b7a44f5481ef87e775afa3505f31ad
suyu06 <suyu.zhang@johnabbottcollege.net>
2023-02-13 14:43:54 -0500

    create useEffect for the loans.tsx

--- src/layouts/ShelfPage/components/Loans.tsx
+++ src/layouts/ShelfPage/components/Loans.tsx
@@ -1,7 +1,7 @@
 import { useOktaAuth } from "@okta/okta-react";
-import { useState } from "react";
+import {  useEffect, useState } from "react";
 import ShelfCurrentLoans from "../../../models/ShelfCurrentLoans";
-
+import { SpinnerLoading } from "../../Utils/SpinnerLoading";
 
 export const Loans = () =>{
     // okta authentication and http error useState
@@ -11,6 +11,57 @@ export const Loans = () =>{
      const [shelfCurrentLoans, setShelfCurrentLoans] = useState<ShelfCurrentLoans[]>([]);
      const [isLoadingUserLoans, setIsLoadingUserLoans] = useState(true);
      const [checkout, setCheckout] = useState(false);
+
+     useEffect(()=>{
+        const  fetchUserCurrentLoans = async () => {
+            //if  user is authenticated, 
+            if (authState && authState.isAuthenticated) {           
+             
+                const url = `http://localhost:8080/api/books/secure/currentloans`;
+                const requestOptions = {
+                    method: 'GET',
+                    headers: {
+                        Authorization: `Bearer ${authState.accessToken?.accessToken}`,
+                        'Content-Type': 'application/json'
+                    }
+                };
+                //fetch data with url and request url and request headers
+                const shelfCurrentLoansResponse = await fetch(url, requestOptions);
+                //if api call fails
+                if (!shelfCurrentLoansResponse.ok) {
+                    throw new Error('Something went wrong!');
+                }
+                //convert to json object
+                const shelfCurrentLoansResponseJson = await shelfCurrentLoansResponse.json();
+                setShelfCurrentLoans(shelfCurrentLoansResponseJson);
+            }
+            // fetching data finished, turn off the loading process
+            setIsLoadingUserLoans(false);
+        }
+    
+     fetchUserCurrentLoans().catch((error: any) => {
+        setIsLoadingUserLoans(false);
+        setHttpError(error.message);
+    })
+    //each time this use effect is called, scroll to the top of the page.
+    window.scrollTo(0, 0);
+},[authState])
+
+if (isLoadingUserLoans) {
+    return (
+        <SpinnerLoading/>
+    );
+}
+
+if (httpError) {
+    return (
+        <div className='container m-5'>
+            <p>
+                {httpError}
+            </p>
+        </div>
+    );
+}
 
     return(
 
