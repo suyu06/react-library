80d8b3f4b0984d752cf132d46f15b40621777421
suyu06 <suyu.zhang@johnabbottcollege.net>
2023-02-13 23:03:16 -0500

    fill in HistoryPage useEffect with async function

--- src/layouts/ShelfPage/components/HistoryPage.tsx
+++ src/layouts/ShelfPage/components/HistoryPage.tsx
@@ -19,6 +19,31 @@ export const HistoryPage = () =>{
     // useEffect
     useEffect (() => {
         const fetchUserHistory = async () => {
+            // only user is authenticated
+            if (authState && authState.isAuthenticated) {
+                const url =
+                 `http://localhost:8080/api/histories/search/findBooksByUserEmail/?userEmail
+                 =${authState.accessToken?.claims.sub}&page=${currentPage - 1}&size=5`;
+                const requestOptions = {
+                    method: 'GET',
+                    headers: {
+                        'Content-Type': 'application/json'
+                    }
+                };
+                // fetch data with url and request (get method and json content)
+                const historyResponse = await fetch(url, requestOptions);
+                // if failure
+                if (!historyResponse.ok) {
+                    throw new Error('Something went wrong!');
+                }
+                // convert to json object
+                const historyResponseJson = await historyResponse.json();
+                // get all histories
+                setHistories(historyResponseJson._embedded.histories);
+                // get the total page number
+                setTotalPages(historyResponseJson.page.totalPages);
+            }
+            setIsLoadingHistory(false);
 
         }
         fetchUserHistory().catch((error: any) => {
