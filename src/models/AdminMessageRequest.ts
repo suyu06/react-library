5b622d6aee7d2b45ae7b19808d62e9fb70ac8939
suyu06 <suyu.zhang@johnabbottcollege.net>
2023-02-15 19:22:05 -0500

    create a new request object AdminQuestionRequest in models folder

---
+++ src/models/AdminMessageRequest.ts
@@ -0,0 +1,13 @@
+
+// mapping with the AdminQuestionRequest in our springboot
+class AdminMessageRequest {
+    id: number;
+    response: string;
+
+    constructor(id: number, response: string) {
+        this.id = id;
+        this.response = response;
+    }
+}
+
+export default AdminMessageRequest;
